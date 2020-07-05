7dcd012e0eb7bf764d8f9c95b0fe15fd726b7e34
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-07-05 15:33:07 +0000

    Replace class methods with instance methods in Network

--- lib/docker/api/network.rb
+++ lib/docker/api/network.rb
@@ -2,42 +2,42 @@ module Docker
     module API        
         class Network < Docker::API::Base
 
-            def self.base_path
+            def base_path
                 "/networks"
             end
 
-            def self.list params = {}
+            def list params = {}
                 validate Docker::API::InvalidParameter, [:filters], params
-                connection.get(build_path("/networks", params))
+                @connection.get(build_path("/networks", params))
             end
 
-            def self.inspect name, params = {}
+            def inspect name, params = {}
                 validate Docker::API::InvalidParameter, [:verbose, :scope], params
-                connection.get(build_path([name], params))
+                @connection.get(build_path([name], params))
             end
 
-            def self.create body = {}
+            def create body = {}
                 validate Docker::API::InvalidRequestBody, [:Name, :CheckDuplicate, :Driver, :Internal, :Attachable, :Ingress, :IPAM, :EnableIPv6, :Options, :Labels], body
-                connection.request(method: :post, path: build_path(["create"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path(["create"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
-            def self.remove name
-                connection.delete(build_path([name]))
+            def remove name
+                @connection.delete(build_path([name]))
             end
 
-            def self.prune params = {}
+            def prune params = {}
                 validate Docker::API::InvalidParameter, [:filters], params
-                connection.post(build_path(["prune"], params))
+                @connection.post(build_path(["prune"], params))
             end
 
-            def self.connect name, body = {}
+            def connect name, body = {}
                 validate Docker::API::InvalidRequestBody, [:Container, :EndpointConfig], body
-                connection.request(method: :post, path: build_path([name, "connect"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path([name, "connect"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
-            def self.disconnect name, body = {}
+            def disconnect name, body = {}
                 validate Docker::API::InvalidRequestBody, [:Container, :Force], body
-                connection.request(method: :post, path: build_path([name, "disconnect"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path([name, "disconnect"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
         end
--- spec/endpoints/network_spec.rb
+++ spec/endpoints/network_spec.rb
@@ -1,28 +1,29 @@
 RSpec.describe Docker::API::Network do
 
-    describe "::list" do 
-        it { expect(described_class.list.status).to eq(200) }
-        it { expect(described_class.list(filters: { dangling: {"true": true} }).status).to eq(200) }
-        it { expect(described_class.list(filters: { driver: {"bridge": true} }).status).to eq(200) }
-        it { expect(described_class.list(filters: { name: {"bridge": true} }).status).to eq(200) }
-        it { expect(described_class.list(filters: { scope: {"local": true} }).status).to eq(200) }
-        it { expect(described_class.list(filters: { type: {"custom": true} }).status).to eq(200) }
-        it { expect(described_class.list(filters: { type: {"builtin": true} }).status).to eq(200) }
-        it { expect{described_class.list( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
+    subject { described_class.new }
+    describe ".list" do 
+        it { expect(subject.list.status).to eq(200) }
+        it { expect(subject.list(filters: { dangling: {"true": true} }).status).to eq(200) }
+        it { expect(subject.list(filters: { driver: {"bridge": true} }).status).to eq(200) }
+        it { expect(subject.list(filters: { name: {"bridge": true} }).status).to eq(200) }
+        it { expect(subject.list(filters: { scope: {"local": true} }).status).to eq(200) }
+        it { expect(subject.list(filters: { type: {"custom": true} }).status).to eq(200) }
+        it { expect(subject.list(filters: { type: {"builtin": true} }).status).to eq(200) }
+        it { expect{subject.list( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
     end
 
-    describe "::inspect" do 
-        it { expect(described_class.inspect( "bridge" ).status).to eq(200) }
-        it { expect(described_class.inspect( "doesn-exist" ).status).to eq(404) }
-        it { expect(described_class.inspect( "bridge", verbose: true ).status).to eq(200) }
-        it { expect(described_class.inspect( "bridge", scope: "local" ).status).to eq(200) }
-        it { expect{described_class.inspect( "bridge", invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
+    describe ".inspect" do 
+        it { expect(subject.inspect( "bridge" ).status).to eq(200) }
+        it { expect(subject.inspect( "doesn-exist" ).status).to eq(404) }
+        it { expect(subject.inspect( "bridge", verbose: true ).status).to eq(200) }
+        it { expect(subject.inspect( "bridge", scope: "local" ).status).to eq(200) }
+        it { expect{subject.inspect( "bridge", invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
     end
 
-    describe "::create" do 
-        it { expect(described_class.create.status).to eq(500) }
+    describe ".create" do 
+        it { expect(subject.create.status).to eq(500) }
         it do 
-            expect(described_class.create( 
+            expect(subject.create( 
                 Name: "rspec-network",
                 CheckDuplicate: true,
                 Driver: "bridge",
@@ -31,48 +32,48 @@ RSpec.describe Docker::API::Network do
                 EnableIPv6: false
                 ).status).to eq(201)
         end
-        it { expect{described_class.create( invalid: true )}.to raise_error(Docker::API::InvalidRequestBody) }
+        it { expect{subject.create( invalid: true )}.to raise_error(Docker::API::InvalidRequestBody) }
         
     end
 
     context "having a container to connect" do
         before(:all) do
-            Docker::API::Image.create(fromImage: "busybox:1.31.1-uclibc")
-            Docker::API::Container.create({name: "rspec-container"}, {Image: "busybox:1.31.1-uclibc"} )
+            Docker::API::Image.new.create(fromImage: "busybox:1.31.1-uclibc")
+            Docker::API::Container.new.create({name: "rspec-container"}, {Image: "busybox:1.31.1-uclibc"} )
         end
         after(:all) do
-            Docker::API::Container.remove("rspec-container")
-            Docker::API::Image.remove("busybox:1.31.1-uclibc")
+            Docker::API::Container.new.remove("rspec-container")
+            Docker::API::Image.new.remove("busybox:1.31.1-uclibc")
         end
 
-        describe "::connect" do 
-            it { expect(described_class.connect("rspec-network").status).to eq(400) }
-            it { expect(described_class.connect("rspec-network", Container: "rspec-container").status).to eq(200) }
-            it { expect{described_class.connect( "rspec-network",  invalid: true )}.to raise_error(Docker::API::InvalidRequestBody) }
+        describe ".connect" do 
+            it { expect(subject.connect("rspec-network").status).to eq(400) }
+            it { expect(subject.connect("rspec-network", Container: "rspec-container").status).to eq(200) }
+            it { expect{subject.connect( "rspec-network",  invalid: true )}.to raise_error(Docker::API::InvalidRequestBody) }
         end
 
-        describe "::disconnect" do 
-            it { expect(described_class.disconnect("rspec-network").status).to eq(400) }
+        describe ".disconnect" do 
+            it { expect(subject.disconnect("rspec-network").status).to eq(400) }
             it do
-                described_class.connect("rspec-network", Container: "rspec-container")
-                expect(described_class.disconnect("rspec-network", Container: "rspec-container", Force: true).status).to eq(200)
+                subject.connect("rspec-network", Container: "rspec-container")
+                expect(subject.disconnect("rspec-network", Container: "rspec-container", Force: true).status).to eq(200)
             end
-            it { expect(described_class.disconnect("rspec-network", Container: "doesn-exist").status).to eq(404) }
-            it { expect(described_class.disconnect("doesn-exist", Container: "rspec-container").status).to eq(500) }
-            it { expect{described_class.disconnect( "rspec-network",  invalid: true )}.to raise_error(Docker::API::InvalidRequestBody)   }
+            it { expect(subject.disconnect("rspec-network", Container: "doesn-exist").status).to eq(404) }
+            it { expect(subject.disconnect("doesn-exist", Container: "rspec-container").status).to eq(500) }
+            it { expect{subject.disconnect( "rspec-network",  invalid: true )}.to raise_error(Docker::API::InvalidRequestBody)   }
         end
     end
 
-    describe "::remove" do 
-        it { expect(described_class.remove( "rspec-network" ).status).to eq(204) }
-        it { expect(described_class.remove( "doesn-exist" ).status).to eq(404) }
+    describe ".remove" do 
+        it { expect(subject.remove( "rspec-network" ).status).to eq(204) }
+        it { expect(subject.remove( "doesn-exist" ).status).to eq(404) }
     end
 
-    describe "::prune" do 
-        it { expect(described_class.prune.status).to eq(200) }
-        it { expect(described_class.prune(filters: { until: {"10m": true} }).status).to eq(200) }
-        it { expect(described_class.prune(filters: { until: {"1h30m": true} }).status).to eq(200) }
-        it { expect(described_class.prune(filters: { label: {"key=value": true} }).status).to eq(200) }
-        it { expect{described_class.prune( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
+    describe ".prune" do 
+        it { expect(subject.prune.status).to eq(200) }
+        it { expect(subject.prune(filters: { until: {"10m": true} }).status).to eq(200) }
+        it { expect(subject.prune(filters: { until: {"1h30m": true} }).status).to eq(200) }
+        it { expect(subject.prune(filters: { label: {"key=value": true} }).status).to eq(200) }
+        it { expect{subject.prune( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
     end
 end
