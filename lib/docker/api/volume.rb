d7e160a432e132907955756239b1022d7a7b3742
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-07-05 15:33:29 +0000

    Replace class methods with instance methods in Volume

--- lib/docker/api/volume.rb
+++ lib/docker/api/volume.rb
@@ -1,32 +1,32 @@
 module Docker
     module API
         class Volume < Docker::API::Base
-            def self.base_path
+            def base_path
                 "/volumes"
             end
 
-            def self.list params = {}
+            def list params = {}
                 validate Docker::API::InvalidParameter, [:filters], params
-                connection.get(build_path("/volumes", params))
+                @connection.get(build_path("/volumes", params))
             end
 
-            def self.create body = {}
+            def create body = {}
                 validate Docker::API::InvalidRequestBody, [:Name, :Driver, :DriverOpts, :Labels], body
-                connection.request(method: :post, path: build_path(["create"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path(["create"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
-            def self.inspect name
-                connection.get(build_path([name]))
+            def inspect name
+                @connection.get(build_path([name]))
             end
 
-            def self.remove name, params = {}
+            def remove name, params = {}
                 validate Docker::API::InvalidParameter, [:force], params
-                connection.delete(build_path([name]))
+                @connection.delete(build_path([name]))
             end
 
-            def self.prune params = {}
+            def prune params = {}
                 validate Docker::API::InvalidParameter, [:filters], params
-                connection.post(build_path(["prune"], params))
+                @connection.post(build_path(["prune"], params))
             end
 
         end
--- spec/endpoints/volume_spec.rb
+++ spec/endpoints/volume_spec.rb
@@ -1,53 +1,55 @@
 RSpec.describe Docker::API::Volume do
     volume = "rspec-volume"
 
-    describe "::list" do
-        it { expect(described_class.list.status).to eq(200) }
-        it { expect(described_class.list(filters: {dangling: {"true": true}}).status).to eq(200) }
-        it { expect(described_class.list(filters: {driver: {"local": true}}).status).to eq(200) }
-        it { expect(described_class.list(filters: {name: {"bridge": true}}).status).to eq(200) }
-        it { expect{described_class.list( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
-        it { expect(described_class.list(filters: {invalid: {"true": true}}).status).to eq(400) }
+    subject { described_class.new }
+
+    describe ".list" do
+        it { expect(subject.list.status).to eq(200) }
+        it { expect(subject.list(filters: {dangling: {"true": true}}).status).to eq(200) }
+        it { expect(subject.list(filters: {driver: {"local": true}}).status).to eq(200) }
+        it { expect(subject.list(filters: {name: {"bridge": true}}).status).to eq(200) }
+        it { expect{subject.list( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
+        it { expect(subject.list(filters: {invalid: {"true": true}}).status).to eq(400) }
     end
 
-    describe "::create" do
+    describe ".create" do
         context "no name given" do
-            subject { described_class.create }
+            subject { described_class.new.create }
             it { expect(subject.status).to eq(201) }
             it { expect(subject.json["Name"]).not_to eq(nil) }
         end
-        it { expect(described_class.create(Name: volume, Driver: "local").status).to eq(201) }
-        it { expect{described_class.create( invalid: true )}.to raise_error(Docker::API::InvalidRequestBody) }
+        it { expect(subject.create(Name: volume, Driver: "local").status).to eq(201) }
+        it { expect{subject.create( invalid: true )}.to raise_error(Docker::API::InvalidRequestBody) }
     end
 
-    describe "::inspect" do
-        it { expect(described_class.inspect(volume).status).to eq(200) }
-        it { expect(described_class.inspect("doesn-exist").status).to eq(404) }    
+    describe ".inspect" do
+        it { expect(subject.inspect(volume).status).to eq(200) }
+        it { expect(subject.inspect("doesn-exist").status).to eq(404) }    
     end
 
-    describe "::remove" do
-        before(:each) { described_class.create(Name: volume, Driver: "local") }
+    describe ".remove" do
+        before(:each) { subject.create(Name: volume, Driver: "local") }
         context "with container attached" do
             before(:all) do
-                Docker::API::Image.create(fromImage: "busybox:1.31.1-uclibc")
-                Docker::API::Container.create({name: "rspec-container"}, {Image: "busybox:1.31.1-uclibc", HostConfig: {Binds: ["#{volume}:/home"]}})
+                Docker::API::Image.new.create(fromImage: "busybox:1.31.1-uclibc")
+                Docker::API::Container.new.create({name: "rspec-container"}, {Image: "busybox:1.31.1-uclibc", HostConfig: {Binds: ["#{volume}:/home"]}})
             end
             after(:all) do
-                Docker::API::Container.remove("rspec-container")
-                Docker::API::Image.remove("busybox:1.31.1-uclibc")
+                Docker::API::Container.new.remove("rspec-container")
+                Docker::API::Image.new.remove("busybox:1.31.1-uclibc")
             end
-            it { expect(described_class.remove(volume).status).to eq(409) }
-            it { expect(described_class.remove(volume, force: true).status).to eq(409) }
+            it { expect(subject.remove(volume).status).to eq(409) }
+            it { expect(subject.remove(volume, force: true).status).to eq(409) }
         end
-        it { expect(described_class.remove(volume).status).to eq(204) }
-        it { expect(described_class.remove("doesn-exist").status).to eq(404) }
-        it { expect{described_class.remove( volume, invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
+        it { expect(subject.remove(volume).status).to eq(204) }
+        it { expect(subject.remove("doesn-exist").status).to eq(404) }
+        it { expect{subject.remove( volume, invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
     end
 
-    describe "::prune" do
-        it { expect(described_class.prune.status).to eq(200) }
-        it { expect(described_class.prune( filters: {label: {"key": true}} ).status).to eq(200) }
-        it { expect(described_class.prune( filters: {label: {"key=value": true}} ).status).to eq(200) }
-        it { expect{described_class.prune( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
+    describe ".prune" do
+        it { expect(subject.prune.status).to eq(200) }
+        it { expect(subject.prune( filters: {label: {"key": true}} ).status).to eq(200) }
+        it { expect(subject.prune( filters: {label: {"key=value": true}} ).status).to eq(200) }
+        it { expect{subject.prune( invalid: true )}.to raise_error(Docker::API::InvalidParameter) }
     end
 end
