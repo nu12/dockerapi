6436400631e868c47ef2b20adb8658010a18ef2e
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-07-08 15:35:17 +0000

    Remove base_path from Swarm

--- lib/docker/api/swarm.rb
+++ lib/docker/api/swarm.rb
@@ -1,42 +1,39 @@
 module Docker
     module API
         class Swarm < Docker::API::Base
-            def base_path
-                "/swarm"
-            end
 
             def init body = {}
                 validate Docker::API::InvalidRequestBody, [:ListenAddr, :AdvertiseAddr, :DataPathAddr, :DataPathPort, :DefaultAddrPool, :ForceNewCluster, :SubnetSize, :Spec], body
-                @connection.request(method: :post, path: build_path(["init"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path("/swarm/init"), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
             def update params = {}, body = {}
                 validate Docker::API::InvalidParameter, [:version, :rotateWorkerToken, :rotateManagerToken, :rotateManagerUnlockKey], params
                 validate Docker::API::InvalidRequestBody, [:Name, :Labels, :Orchestration, :Raft, :Dispatcher, :CAConfig, :EncryptionConfig, :TaskDefaults], body
-                @connection.request(method: :post, path: build_path(["update"], params), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path("/swarm/update", params), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
             def inspect
-                @connection.get(base_path)
+                @connection.get("/swarm")
             end
 
             def unlock_key
-                @connection.get(build_path(["unlockkey"]))
+                @connection.get(build_path("/swarm/unlockkey"))
             end
 
             def unlock body = {}
                 validate Docker::API::InvalidRequestBody, [:UnlockKey], body
-                @connection.request(method: :post, path: build_path(["unlock"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path("/swarm/unlock"), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
             def join body = {}
                 validate Docker::API::InvalidRequestBody, [:ListenAddr, :AdvertiseAddr, :DataPathAddr, :RemoteAddrs, :JoinToken], body
-                @connection.request(method: :post, path: build_path(["join"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
+                @connection.request(method: :post, path: build_path("/swarm/join"), headers: {"Content-Type": "application/json"}, body: body.to_json)
             end
 
             def leave params = {}
                 validate Docker::API::InvalidParameter, [:force], params
-                @connection.post(build_path(["leave"], params))
+                @connection.post(build_path("/swarm/leave", params))
             end
         end
     end
