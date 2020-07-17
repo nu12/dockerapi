091b3ba28f270276ad630a031ab7069c9c5f957d
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-07-17 13:00:02 +0000

    Service doc

--- lib/docker/api/service.rb
+++ lib/docker/api/service.rb
@@ -1,42 +1,98 @@
-module Docker
-    module API
-        class Service < Docker::API::Base
+# This class represents the Docker API endpoints regarding services.
+#
+# @see https://docs.docker.com/engine/api/v1.40/#tag/Service
+#
+# Services are the definitions of tasks to run on a swarm. Swarm mode must be enabled for these endpoints to work.
+class Docker::API::Service < Docker::API::Base
 
-            def list params = {}
-                validate Docker::API::InvalidParameter, [:filters], params
-                @connection.get(build_path("/services", params))
-            end
+    # List services
+    #
+    # Docker API: GET /services
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/ServiceList
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
+    def list params = {}
+        validate Docker::API::InvalidParameter, [:filters], params
+        @connection.get(build_path("/services", params))
+    end
 
-            def create body = {}, authentication = {}
-                validate Docker::API::InvalidRequestBody, [:Name, :Labels, :TaskTemplate, :Mode, :UpdateConfig, :RollbackConfig, :Networks, :EndpointSpec], body
-                headers = {"Content-Type": "application/json"}
-                headers.merge!({"X-Registry-Auth" => Base64.urlsafe_encode64(authentication.to_json.to_s)}) if authentication.keys.size > 0
-                @connection.request(method: :post, path: "/services/create", headers: headers, body: body.to_json)
-            end
+    # Create a service
+    #
+    # Docker API: POST /services/create
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/ServiceCreate
+    #
+    # @param body [Hash]: Request body to be sent as json.
+    #
+    # @param authentication [Hash]: Authentication parameters.
+    def create body = {}, authentication = {}
+        validate Docker::API::InvalidRequestBody, [:Name, :Labels, :TaskTemplate, :Mode, :UpdateConfig, :RollbackConfig, :Networks, :EndpointSpec], body
+        headers = {"Content-Type": "application/json"}
+        headers.merge!({"X-Registry-Auth" => Base64.urlsafe_encode64(authentication.to_json.to_s)}) if authentication.keys.size > 0
+        @connection.request(method: :post, path: "/services/create", headers: headers, body: body.to_json)
+    end
 
-            def update name, params = {}, body = {}, authentication = {}
-                # view https://github.com/docker/swarmkit/issues/1394#issuecomment-240850719
-                validate Docker::API::InvalidParameter, [:version, :registryAuthFrom, :rollback], params
-                validate Docker::API::InvalidRequestBody, [:Name, :Labels, :TaskTemplate, :Mode, :UpdateConfig, :RollbackConfig, :Networks, :EndpointSpec], body
-                headers = {"Content-Type": "application/json"}
-                headers.merge!({"X-Registry-Auth" => Base64.urlsafe_encode64(authentication.to_json.to_s)}) if authentication.keys.size > 0
-                @connection.request(method: :post, path: build_path("/services/#{name}/update", params), headers: headers, body: body.to_json)
-            end
+    # Update a service
+    #
+    # Docker API: POST /services/{id}/update
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/ServiceUpdate
+    #
+    # @param name [String]: The ID or name of the service.
+    #
+    # @param params [Hash]:  Parameters that are appended to the URL.
+    #
+    # @param body [Hash]: Request body to be sent as json.
+    #
+    # @param authentication [Hash]:  Authentication parameters.
+    def update name, params = {}, body = {}, authentication = {}
+        # view https://github.com/docker/swarmkit/issues/1394#issuecomment-240850719
+        validate Docker::API::InvalidParameter, [:version, :registryAuthFrom, :rollback], params
+        validate Docker::API::InvalidRequestBody, [:Name, :Labels, :TaskTemplate, :Mode, :UpdateConfig, :RollbackConfig, :Networks, :EndpointSpec], body
+        headers = {"Content-Type": "application/json"}
+        headers.merge!({"X-Registry-Auth" => Base64.urlsafe_encode64(authentication.to_json.to_s)}) if authentication.keys.size > 0
+        @connection.request(method: :post, path: build_path("/services/#{name}/update", params), headers: headers, body: body.to_json)
+    end
 
-            def details name, params = {}
-                validate Docker::API::InvalidParameter, [:insertDefaults], params
-                @connection.get(build_path("/services/#{name}", params))
-            end
+    # Inspect a service
+    #
+    # Docker API: GET /services/{id}
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/ServiceInspect
+    #
+    # @param name [String]: The ID or name of the service.
+    #
+    # @param params [Hash]: Parameters that are appended to the URL. 
+    def details name, params = {}
+        validate Docker::API::InvalidParameter, [:insertDefaults], params
+        @connection.get(build_path("/services/#{name}", params))
+    end
 
-            def logs name, params = {}
-                validate Docker::API::InvalidParameter, [:details, :follow, :stdout, :stderr, :since, :timestamps, :tail], params
-                @connection.get(build_path("/services/#{name}/logs", params))
-            end
+    # Get stdout and stderr logs from a service. 
+    #
+    # Note: This endpoint works only for services with the local, json-file or journald logging drivers.
+    #
+    # Docker API: GET /services/{id}/logs
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/ServiceLogs
+    #
+    # @param name [String]: The ID or name of the service.
+    #
+    # @param params [Hash]: Parameters that are appended to the URL. 
+    def logs name, params = {}
+        validate Docker::API::InvalidParameter, [:details, :follow, :stdout, :stderr, :since, :timestamps, :tail], params
+        @connection.get(build_path("/services/#{name}/logs", params))
+    end
 
-            def delete name
-                @connection.delete("/services/#{name}")
-            end
-
-        end
+    # Delete a service
+    #
+    # Docker API: DELETE /services/{id}
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/ServiceDelete
+    #
+    # @param name [String]: The ID or name of the service.
+    def delete name
+        @connection.delete("/services/#{name}")
     end
 end
