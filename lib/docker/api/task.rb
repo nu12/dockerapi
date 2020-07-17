e2cba9c185c9983a0e309f616ea0abcc5dacd846
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-07-17 12:34:16 +0000

    Add Task doc

--- lib/docker/api/task.rb
+++ lib/docker/api/task.rb
@@ -1,21 +1,36 @@
-module Docker
-    module API
-        class Task < Docker::API::Base
+# This class represents the Docker API endpoints regarding tasks.
+# https://docs.docker.com/engine/api/v1.40/#tag/Task
+# A task is a container running on a swarm. It is the atomic scheduling unit of swarm. Swarm mode must be enabled for these endpoints to work.
+class Docker::API::Task < Docker::API::Base
 
-            def list params = {}
-                validate Docker::API::InvalidParameter, [:filters], params
-                @connection.get(build_path("/tasks",params))
-            end
+    # List tasks
+    # Docker API: GET /tasks
+    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/TaskList
+    #
+    # @param params (Hash) : Parameters that are appended to the URL.
+    def list params = {}
+        validate Docker::API::InvalidParameter, [:filters], params
+        @connection.get(build_path("/tasks",params))
+    end
 
-            def details name
-                @connection.get("/tasks/#{name}")
-            end
+    # Inspect a task
+    # Docker API: GET /tasks/{id}
+    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/TaskInspect
+    #
+    # @param name (String) : The ID or name of the task.
+    def details name
+        @connection.get("/tasks/#{name}")
+    end
 
-            def logs name, params = {}
-                validate Docker::API::InvalidParameter, [:details, :follow, :stdout, :stderr, :since, :timestamps, :tail], params
-                @connection.get(build_path("/tasks/#{name}/logs", params))
-            end
-
-        end
+    # Get stdout and stderr logs from a task.
+    # Note: This endpoint works only for services with the local, json-file or journald logging drivers.
+    # Docker API: GET /tasks/{id}/logs
+    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/TaskLogs
+    #
+    # @param name (String) : The ID or name of the task.
+    # @param params (Hash) : Parameters that are appended to the URL.
+    def logs name, params = {}
+        validate Docker::API::InvalidParameter, [:details, :follow, :stdout, :stderr, :since, :timestamps, :tail], params
+        @connection.get(build_path("/tasks/#{name}/logs", params))
     end
 end
