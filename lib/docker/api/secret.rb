b11085dedd4d428e8a26bc2695663c4131c37455
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-07-17 13:04:31 +0000

    Refactoring doc

--- lib/docker/api/secret.rb
+++ lib/docker/api/secret.rb
@@ -1,44 +1,56 @@
 # This class represents the Docker API endpoints regarding secrets.
-# https://docs.docker.com/engine/api/v1.40/#tag/Secret
+#
+# @see https://docs.docker.com/engine/api/v1.40/#tag/Secret
+#
 # Secrets are sensitive data that can be used by services. Swarm mode must be enabled for these endpoints to work.
 class Docker::API::Secret < Docker::API::Base
 
     # List secrets
-    # Docker API: GET /secrets
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/SecretList
     #
-    # @param params (Hash) : Parameters that are appended to the URL.
+    # Docker API: GET /secrets
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/SecretList
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
     def list params = {}
         validate Docker::API::InvalidParameter, [:filters], params
         @connection.get(build_path("/secrets",params))
     end
 
     # Create a secret
-    # Docker API: POST /secrets/create
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/SecretCreate
     #
-    # @param body (Hash) : Request body to be sent as json.
+    # Docker API: POST /secrets/create
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/SecretCreate
+    #
+    # @param body [Hash]: Request body to be sent as json.
     def create body = {}
         validate Docker::API::InvalidRequestBody, [:Name, :Labels, :Data, :Driver, :Templating], body
         @connection.request(method: :post, path: "/secrets/create", headers: {"Content-Type": "application/json"}, body: body.to_json)
     end
 
     # Inspect a secret
-    # Docker API: GET /secrets/{id}
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/SecretInspect
     #
-    # @param name (String) : The ID or name of the secret.
+    # Docker API: GET /secrets/{id}
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/SecretInspect
+    #
+    # @param name [String]: The ID or name of the secret.
     def details name
         @connection.get("/secrets/#{name}")
     end
 
     # Update a secret
-    # Docker API: POST /secrets/{id}/update
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/SecretUpdate
     #
-    # @param name (String) : The ID or name of the secret.
-    # @param params (Hash) : Parameters that are appended to the URL.
-    # @param body (Hash) : Request body to be sent as json.
+    # Docker API: POST /secrets/{id}/update
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/SecretUpdate
+    #
+    # @param name [String]: The ID or name of the secret.
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
+    #
+    # @param body [Hash]: Request body to be sent as json.
     def update name, params = {}, body = {}
         validate Docker::API::InvalidParameter, [:version], params
         validate Docker::API::InvalidRequestBody, [:Name, :Labels, :Data, :Driver, :Templating], body
@@ -46,10 +58,12 @@ class Docker::API::Secret < Docker::API::Base
     end
 
     # Delete a secret
-    # Docker API: DELETE /secrets/{id}
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/SecretDelete
     #
-    # @param name (String) : The ID or name of the secret.
+    # Docker API: DELETE /secrets/{id}
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/SecretDelete
+    #
+    # @param name [String]: The ID or name of the secret.
     def delete name
         @connection.delete("/secrets/#{name}")
     end
--- lib/docker/api/task.rb
+++ lib/docker/api/task.rb
@@ -1,33 +1,43 @@
 # This class represents the Docker API endpoints regarding tasks.
-# https://docs.docker.com/engine/api/v1.40/#tag/Task
+#
+# @see https://docs.docker.com/engine/api/v1.40/#tag/Task
+#
 # A task is a container running on a swarm. It is the atomic scheduling unit of swarm. Swarm mode must be enabled for these endpoints to work.
 class Docker::API::Task < Docker::API::Base
 
     # List tasks
-    # Docker API: GET /tasks
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/TaskList
     #
-    # @param params (Hash) : Parameters that are appended to the URL.
+    # Docker API: GET /tasks
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/TaskList
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
     def list params = {}
         validate Docker::API::InvalidParameter, [:filters], params
         @connection.get(build_path("/tasks",params))
     end
 
     # Inspect a task
-    # Docker API: GET /tasks/{id}
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/TaskInspect
     #
-    # @param name (String) : The ID or name of the task.
+    # Docker API: GET /tasks/{id}
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/TaskInspect
+    #
+    # @param name [String]: The ID or name of the task.
     def details name
         @connection.get("/tasks/#{name}")
     end
 
     # Get stdout and stderr logs from a task.
+    #
     # Note: This endpoint works only for services with the local, json-file or journald logging drivers.
+    #
     # Docker API: GET /tasks/{id}/logs
-    # Docker Doc: https://docs.docker.com/engine/api/v1.40/#operation/TaskLogs
+    #
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/TaskLogs
     #
     # @param name (String) : The ID or name of the task.
+    #
     # @param params (Hash) : Parameters that are appended to the URL.
     def logs name, params = {}
         validate Docker::API::InvalidParameter, [:details, :follow, :stdout, :stderr, :since, :timestamps, :tail], params
