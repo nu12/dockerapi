696ee927c11397ca7be28272d2e8900109ad1862
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-08-06 16:49:02 +0000

    Add documentation to Network

--- lib/docker/api/network.rb
+++ lib/docker/api/network.rb
@@ -1,50 +1,86 @@
-module Docker
-    module API        
-        class Network < Docker::API::Base
+##
+# This class represents the Docker API endpoints regarding networks.
+# @see https://docs.docker.com/engine/api/v1.40/#tag/Network
+class Docker::API::Network < Docker::API::Base
 
-            def list params = {}
-                @connection.get(build_path("/networks", params))
-            end
-
-            def details name, params = {}
-                @connection.get(build_path([name], params))
-            end
-
-            def create body = {}
-                @connection.request(method: :post, path: build_path(["create"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
-            end
-
-            def remove name
-                @connection.delete(build_path([name]))
-            end
-
-            def prune params = {}
-                @connection.post(build_path(["prune"], params))
-            end
-
-            def connect name, body = {}
-                @connection.request(method: :post, path: build_path([name, "connect"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
-            end
-
-            def disconnect name, body = {}
-                @connection.request(method: :post, path: build_path([name, "disconnect"]), headers: {"Content-Type": "application/json"}, body: body.to_json)
-            end
-
-            #################################################
-            # Items in this area to be removed before 1.0.0 #
-            #################################################
-            def base_path
-                "/networks"
-            end
-
-            def inspect *args
-                return super.inspect if args.size == 0
-                warn  "WARNING: #inspect is deprecated and will be removed in the future, please use #details instead."
-                name, params = args[0], args[1] || {}
-                details(name, params)
-            end
-            #################################################
-
-        end
+    ##
+    # List networks.
+    #
+    # Docker API: GET /networks
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkList
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
+    def list params = {}
+        @connection.get(build_path("/networks", params))
     end
+
+    ##
+    # Inspect a network.
+    #
+    # Docker API: GET /networks/{id}
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkInspect
+    #
+    # @param name [String]: The ID or name of the network.
+    # @param params [Hash]: Parameters that are appended to the URL.
+    def details name, params = {}
+        @connection.get(build_path("/networks/#{name}", params))
+    end
+
+    ##
+    # Create a network.
+    #
+    # Docker API: POST /networks/create
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkCreate
+    #
+    # @param body [Hash]: Request body to be sent as json.
+    def create body = {}
+        @connection.request(method: :post, path: "/networks/create", headers: {"Content-Type": "application/json"}, body: body.to_json)
+    end
+
+    ##
+    # Remove a network.
+    #
+    # Docker API: DELETE /networks/{id}
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkDelete
+    #
+    # @param name [String]: The ID or name of the network.
+    def remove name
+        @connection.delete("/networks/#{name}")
+    end
+
+    ##
+    # Delete unused networks.
+    #
+    # Docker API: POST /networks/prune
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkPrune
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
+    def prune params = {}
+        @connection.post(build_path("/networks/prune", params))
+    end
+
+    ##
+    # Connect a container to a network.
+    #
+    # Docker API: POST /networks/{id}/connect
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkConnect
+    #
+    # @param name [String]: The ID or name of the network.
+    # @param body [Hash]: Request body to be sent as json.
+    def connect name, body = {}
+        @connection.request(method: :post, path: "/networks/#{name}/connect", headers: {"Content-Type": "application/json"}, body: body.to_json)
+    end
+
+    ##
+    # Disconnect a container from a network.
+    #
+    # Docker API: POST /networks/{id}/disconnect
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NetworkDisconnect
+    #
+    # @param name [String]: The ID or name of the network.
+    # @param body [Hash]: Request body to be sent as json.
+    def disconnect name, body = {}
+        @connection.request(method: :post, path: "/networks/#{name}/disconnect", headers: {"Content-Type": "application/json"}, body: body.to_json)
+    end
+
 end
