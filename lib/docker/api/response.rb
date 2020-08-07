8aa54ee6a7167551f480b152ac065bc7b03c5d9b
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-08-07 18:19:44 +0000

    Refactoring Response

--- lib/docker/api/response.rb
+++ lib/docker/api/response.rb
@@ -1,25 +1,33 @@
-module Docker
-    module API
-        class Response < Excon::Response
-            attr_reader(:json, :path)
+##
+# Reponse class.
+class Docker::API::Response < Excon::Response
+    attr_reader(:json, :path)
 
-            def initialize data
-                super data
-                @json = parse_json @body
-                @path = @data[:path]
-            end
+    ##
+    # Initialize a new Response object.
+    #
+    # @params data [Object]: Reponse's data.
+    def initialize data
+        super data
+        @json = parse_json @body
+        @path = @data[:path]
+    end
 
-            def success?
-                (200..204).include? @status
-            end
+    ##
+    # Returns true if Response status is in 200..204 range.
+    def success?
+        (200..204).include? @status
+    end
 
-            private
+    private
 
-            def parse_json data
-               return nil unless headers["Content-Type"] == "application/json"
-               return nil if data == ""
-               data.split("\r\n").size > 1 ? data.split("\r\n").map{ |e| eval(e) } : JSON.parse(data)
-            end
-        end
+    ##
+    # Creates a json from Response data attribute.
+    #
+    # @params data [String]: String to be converted in json.
+    def parse_json data
+        return nil unless headers["Content-Type"] == "application/json"
+        return nil if data == ""
+        data.split("\r\n").size > 1 ? data.split("\r\n").map{ |e| eval(e) } : JSON.parse(data)
     end
 end
