bfd6c10839d25fb2944cd4075074b155605bed48
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-08-07 14:51:39 +0000

    Add documentation to Node

--- lib/docker/api/node.rb
+++ lib/docker/api/node.rb
@@ -1,34 +1,53 @@
-module Docker
-    module API        
-        class Node < Docker::API::Base
+# This class represents the Docker API endpoints regarding nodes.
+#
+# Nodes are instances of the Engine participating in a swarm. Swarm mode must be enabled for these endpoints to work.
+# @see https://docs.docker.com/engine/api/v1.40/#tag/Node
+class Docker::API::Node < Docker::API::Base
 
-            def list params = {}
-                @connection.get(build_path("/nodes", params))
-            end
+    ##
+    # List nodes.
+    #
+    # Docker API: GET /nodes
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NodeList
+    #
+    # @param params [Hash]: Parameters that are appended to the URL.
+    def list params = {}
+        @connection.get(build_path("/nodes", params))
+    end
 
-            def update name, params = {}, body = {}
-                @connection.request(method: :post, path: build_path("nodes/#{name}/update", params), headers: {"Content-Type": "application/json"}, body: body.to_json)
-            end
+    ##
+    # Update a node.
+    #
+    # Docker API: POST /nodes/{id}/update
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NodeUpdate
+    #
+    # @param name [String]: The ID or name of the node.
+    # @param params [Hash]: Parameters that are appended to the URL.
+    # @param body [Hash]: Request body to be sent as json.
+    def update name, params = {}, body = {}
+        @connection.request(method: :post, path: build_path("nodes/#{name}/update", params), headers: {"Content-Type": "application/json"}, body: body.to_json)
+    end
 
-            def delete name, params = {}
-                @connection.delete(build_path("/nodes/#{name}", params))
-            end
+    ##
+    # Delete a node.
+    #
+    # Docker API: DELETE /nodes/{id}
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NodeDelete
+    #
+    # @param name [String]: The ID or name of the node.
+    # @param params [Hash]: Parameters that are appended to the URL.
+    def delete name, params = {}
+        @connection.delete(build_path("/nodes/#{name}", params))
+    end
 
-            def details name
-                @connection.get("/nodes/#{name}")
-            end
-
-            #################################################
-            # Items in this area to be removed before 1.0.0 #
-            #################################################
-            def inspect *args
-                return super.inspect if args.size == 0
-                warn  "WARNING: #inspect is deprecated and will be removed in the future, please use #details instead."
-                name = args[0]
-                details(name)
-            end
-            #################################################
-
-        end
+    ##
+    # Inspect a node.
+    #
+    # Docker API: GET /nodes/{id}
+    # @see https://docs.docker.com/engine/api/v1.40/#operation/NodeInspect
+    #
+    # @param name [String]: The ID or name of the node.
+    def details name
+        @connection.get("/nodes/#{name}")
     end
 end
