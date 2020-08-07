2eedaa4aa311a38cac05c52d306fdaf50466a3a4
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-08-07 18:02:48 +0000

    Add Base docs

--- lib/docker/api/base.rb
+++ lib/docker/api/base.rb
@@ -1,24 +1,21 @@
+##
+# Base class to provide general methods, helpers and implementations accross classes.
 class Docker::API::Base
     
-
+    ##
+    # Creates new object and sets the validation to happen automatically when method parameters include "params" or "body".
+    #
+    # @param connection [Docker::API::Connection]: Connection to be used.
     def initialize connection = nil
         raise Docker::API::Error.new("Expected connection to be a Docker::API::Connection class") if connection != nil && !connection.is_a?(Docker::API::Connection)
         @connection = connection || Docker::API::Connection.new
-        
-        (self.methods - Object.methods).each do |method|
-            params_index = method(method).parameters.map{|ar| ar[1]}.index(:params)
-            body_index = method(method).parameters.map{|ar| ar[1]}.index(:body)
-
-            define_singleton_method(method) do |*args, &block|
-                validate Docker::API::InvalidParameter, Docker::API::VALID_PARAMS["#{self.class.name}"]["#{method}"], (args[params_index] || {}) if params_index
-                validate Docker::API::InvalidRequestBody, Docker::API::VALID_BODY["#{self.class.name}"]["#{method}"], (args[body_index] || {}) if body_index
-                super(*args,&block)
-            end
-        end
+        set_automated_validation
     end
     
     private
 
+    ##
+    # Outputs to stdout.
     def default_streamer
         streamer = lambda do |chunk, remaining_bytes, total_bytes|
             p chunk.to_s.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?') if Docker::API::PRINT_TO_STDOUT
@@ -26,6 +23,10 @@ class Docker::API::Base
         streamer
     end
 
+    ##
+    # Writes file.
+    #
+    # @param path [String]: Path to the file to be writen.
     def default_writer path
         streamer = lambda do |chunk, remaining_bytes, total_bytes|
             return if "#{chunk}".match(/(No such image)/)
@@ -36,6 +37,13 @@ class Docker::API::Base
         streamer
     end
 
+    ##
+    # Reads file.
+    #
+    # @param path [String]: Path to the file to be read.
+    # @param url [String]: Endpoint URL where the file is going to be sent.
+    # @param header [Hash]: Header of the request.
+    # @param &block: Replace the default output to stdout behavior.
     def default_reader path, url, header = {"Content-Type" => "application/x-tar"}, &block
         file = File.open(File.expand_path(path), "r")
         response = @connection.request(method: :post, path: url , headers: header, request_block: lambda { file.read(Excon.defaults[:chunk_size]).to_s}, response_block: block_given? ? block.call : default_streamer )
@@ -43,28 +51,60 @@ class Docker::API::Base
         response
     end
 
+    ##
+    # Encodes the authentication parameters.
+    #
+    # @param authentication [Hash]: Parameters to be encoded.
     def auth_encoder(authentication)
         Base64.urlsafe_encode64(authentication.to_json.to_s).chomp
     end
 
+    ##
+    # Validates a Hash object comparing its keys with a given Array of permitted values. Raise an error if the validation fail.
+    #
+    # @param error [Error]: Error to be raised of the validation fail.
+    # @param permitted [Array]: List of permitted keys.
+    # @param params [Hash]: Hash object to be validated.
     def validate error, permitted, params
         return if params[:skip_validation]
         unpermitted = params.keys.map(&:to_s) - permitted.map(&:to_s)
         raise error.new(permitted, unpermitted) if unpermitted.size > 0
     end
 
-    ## Converts Ruby Hash into query parameters
-    ## In general, the format is key=value
-    ## If value is another Hash, it should keep a json syntax {key:value}
-    def hash_to_params h
+    ##
+    # Converts Ruby Hash into URL query parameters.
+    #
+    # In general, query parameters' format is "key=value", but if "value" is another Hash, it should change to a json syntax {key:value}.
+    #
+    # @param hash [Hash]: Hash object to be converted in a query parameter-like string.
+    def hash_to_params hash
         p = []
-        h.delete_if{ | k, v | k.to_s == "skip_validation" }.each { |k,v| p.push( v.is_a?(Hash) ? "#{k}=#{v.to_json}" : "#{k}=#{v}") }
+        hash.delete_if{ | k, v | k.to_s == "skip_validation" }.each { |k,v| p.push( v.is_a?(Hash) ? "#{k}=#{v.to_json}" : "#{k}=#{v}") }
         p.join("&").gsub(" ","")
     end
 
+    ##
+    # Builds an URL string using the base path and a set of parameters.
+    #
+    # @param path [String]: Base URL string.
+    # @param hash [Hash]: Hash object to be appended to the URL as query parameters.
     def build_path path, params = {}
-        #p = path.is_a?(Array) ? ([base_path] << path).join("/") : path # TODO: this line to be removed?
         params.size > 0 ? [path, hash_to_params(params)].join("?") : path
+    end
+
+    ##
+    # Sets the validation to happen automatically when method parameters include "params" or "body".
+    def set_automated_validation
+        (self.methods - Object.methods).each do |method|
+            params_index = method(method).parameters.map{|ar| ar[1]}.index(:params)
+            body_index = method(method).parameters.map{|ar| ar[1]}.index(:body)
+
+            define_singleton_method(method) do |*args, &block|
+                validate Docker::API::InvalidParameter, Docker::API::VALID_PARAMS["#{self.class.name}"]["#{method}"], (args[params_index] || {}) if params_index
+                validate Docker::API::InvalidRequestBody, Docker::API::VALID_BODY["#{self.class.name}"]["#{method}"], (args[body_index] || {}) if body_index
+                super(*args,&block)
+            end
+        end
     end
 
 end
