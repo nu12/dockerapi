b6b94995ecf6c7f8cdee7c6bdd5da46509a3564e
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-12-07 15:02:23 -0300

    WIP: adding authentication tests, resolves #4

--- spec/endpoints/image_spec.rb
+++ spec/endpoints/image_spec.rb
@@ -189,4 +189,70 @@ RSpec.describe Docker::API::Image do
         it { expect(subject.distribution(image).status).to eq(200) }
         it { expect(subject.distribution("doesn-exist").status).to eq(403) }
     end
+
+    describe "authentication" do  
+        original = "registry:2.7.0"
+        local = "localhost:5000/janedoe/test:latest"
+        before(:all) do
+            # Download image
+            described_class.new.create(fromImage: original)
+
+            # Create container
+            container = Docker::API::Container.new
+            container.create( {name: "registry"}, {
+                Image: original, 
+                HostConfig: {
+                    PortBindings: {"5000/tcp": [ {HostIp: "0.0.0.0", HostPort: "5000"} ] }, 
+                    Binds: [
+                        "#{File.expand_path(File.dirname(__FILE__))}/../../resources/registry_authentication:/auth",
+                        "#{File.expand_path(File.dirname(__FILE__))}/../../resources/registry_authentication:/certs"]},
+                Env: [
+                    "REGISTRY_AUTH=htpasswd", 
+                    "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm",
+                    "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd",
+                    "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry_auth.crt",
+                    "REGISTRY_HTTP_TLS_KEY=/certs/registry_auth.key",
+
+                ],
+            })
+            # Start container
+            container.start("registry")
+
+            # Create local repository
+            described_class.new.tag(original, repo: local)
+        end
+        
+        #Image#push
+            # Right credential
+            # Wrong credential
+        it { expect(subject.push(local, {}, {username: "janedoe", password: "password"}).status).to eq(200) }
+        it { expect(subject.push(local, {}, {username: "janedoe", password: "password"}).json.last[:aux][:Size]).to be > 0 }
+        it { expect(subject.push(local, {}, {username: "janedoe", password: "wrong-password"}).status).to eq(200) }
+        it { expect(subject.push(local, {}, {username: "janedoe", password: "wrong-password"}).json.last[:error]).to match(/(unauthorized: authentication required)/) }
+        
+        #Image#build
+            # Right credential
+            # Wrong credential
+        #Image#create
+            # Right credential
+                # Unexisting repo
+            # Wrong credential
+        #Image#distribution
+            # Right credential
+                # Unexisting repo
+            # Wrong credential
+    
+        after(:all) do
+            container = Docker::API::Container.new
+            # Stop container
+            container.stop("registry")
+            # Remove container
+            container.remove("registry")
+            # Remove image
+            described_class.new.remove(original)
+            # Remove volumes
+            Docker::API::Volume.new.prune
+        end
+        
+    end
 end
